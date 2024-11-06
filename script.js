c493e4d08cd142bcdff846c1b2d8c3aa3174dca1
Wahrzd12 <shahrizodaabdurahmonova39@gmail.com>
2024-11-06 19:49:54 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let name = prompt("ismingizni kiriting");
+let familya = prompt("familya kiriting");
+let yosh = prompt ("yoshimgizni kiriting")
+let It = prompt ("yoqtirgan faningizni kiriting" ) 
+let tuman = prompt ("yashaydigan tumaningizni kiriting" ) 
+let rahmat = ("ma'lumotlaringiz uchun rahmat" ) 
+alert (rahmat)
